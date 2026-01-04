538eecdd7678ed2369335adc3e88a9ec083e145f
Muhammed-Anshid-Ank <126402590+Muhammed-Anshid-Ank@users.noreply.github.com>
2026-01-04 19:13:57 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:akitra123@localhost:5432/taskflow"
+DATABASE_URL = "postgresql://postgres:123@localhost:5432/taskflow"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
